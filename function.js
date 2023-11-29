b8d3df7a715c854f5df585e0f222555fc70bc2f4
davkaforce <davaadalaio@gmail.com>
2023-11-29 13:04:56 +0800

    justupdate

--- function.js
+++ function.js
@@ -38,14 +38,14 @@
 //    /  - noogdwor
 //    %  - uldegdel
 
-function problem1(a, b) {
-  let result1 = 2 * (a + b);
-  let result2 = a * b;
-  return result1 + result2 - "10";
-}
+// function problem1(a, b) {
+//   let result1 = 2 * (a + b);
+//   let result2 = a * b;
+//   return result1 + result2 - "10";
+// }
 
-let mainresult = problem1(5, 6);
-console.log(mainresult);
+// let mainresult = problem1(5, 6);
+// console.log(mainresult);
 
 // number + number = number
 //
@@ -70,26 +70,70 @@ console.log(mainresult);
 
 // problem classroom
 
-function functionA(num1, num2, operation) {
-  let answer;
-  if (operation == "+") {
-    answer = num1 + num2;
-  }
-  return answer;
-}
+// function functionA(num1, num2, operation) {
+//   let answer;
+//   if (operation == "+") {
+//     answer = num1 + num2;
+//   }
+//   return answer;
+// }
 
-function functionB(num1, num2, operation) {
-  if (operation == "+") {
-    return num1 + num2;
-  }
-}
+// function functionB(num1, num2, operation) {
+//   if (operation == "+") {
+//     return num1 + num2;
+//   }
+// }
 
-resultA = functionA(2, 2, "+");
-resultB = functionB(2, 3, "+");
-console.log("result of functionA is", resultA);
-console.log("result of functionB is", resultB);
+// resultA = functionA(2, 2, "+");
+// resultB = functionB(2, 3, "+");
+// console.log("result of functionA is", resultA);
+// console.log("result of functionB is", resultB);
 
 // scope
 
 // global scope - without any bracket
 // local scope - with curly bracket/
+
+// RGB7009;
+
+// function sumNum(num) {
+//   let newarray = num.split(" ");
+//   return newarray;
+// }
+// console.log(sumNum(32));
+
+// function getNum(num) {
+//   let first = num % 10;
+//   let second = num % 100;
+//   let result = (second - first) / 10;
+//   return result;
+// }
+// let result = getNum(423);
+// console.log(result);
+
+// multiply numbers included in a num
+
+// function multiplyChar(num) {
+//   let numStr = num.toString();
+//   let numArr = numStr.split("");
+//   let nums = numArr.map((n) => parseInt(n));
+//   return nums;
+// }
+// let result = multiplyChar(423);
+// console.log(result);
+
+// const num = 784;
+// const numStr = num.toString();
+// const numArr = numStr.split("");
+// const nums = numArr.map(n => parseInt(n));
+// console.log(nums); // [7, 8, 4]
+
+function hourminsec(num) {
+  hour = (num - (num % 3600)) / 3600;
+  min = ((num % 3600) - ((num % 3600) % 60)) / 60;
+  sec = num % 60;
+
+  final = hour + "hour " + min + "min " + sec + "sec";
+  return final;
+}
+console.log(hourminsec(154434));
